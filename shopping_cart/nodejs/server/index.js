3501ad7dffdcab68959dd9c442da060c3c67a8e1
Stoian Iulia <iuliastoian8@gmail.com>
2022-09-23 08:35:13 +0300

    подключение к базе данных

--- shopping_cart/nodejs/server/index.js
+++ shopping_cart/nodejs/server/index.js
@@ -1,8 +1,20 @@
 require("dotenv").config()
 const express = require('express') //с помощью require можно импортировать какие то модули в файл, в данном случае express
-
+const sequelize = require('./db')  //импорт объекта из файла .db
 const PORT = process.env.PORT || 5000 //получаем порт из переменной окружения, если эта переменная не задана, тогда по умоолчанию будет порт 5000
 
 const app = express() //создаем объект вызвав функцию экспресс, с него бужет начинаться запуск нашего приложения
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`)) //у app вызываем функцию listen, в которой указываем какой порт должен прослушивать наш сервер
+const start = async() =>  {       //асинхронная функция для подключения к базе данных
+
+   // блок try catch чтобы отлавливать возможные ошибки
+    try {
+        await sequelize.authenticate()  //устанавливает подключение к базе данных
+        await sequelize.sync() //сверяет состояние базы данных со схемой данных
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`)) //у app вызываем функцию listen, в которой указываем какой порт должен прослушивать наш сервер
+    } catch (e) {
+        console.log(e) 
+    }
+}
+
+start()
