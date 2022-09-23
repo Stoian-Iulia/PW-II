6a767aa2f93a0adaca4468de221d2113101a1ab1
Stoian Iulia <iuliastoian8@gmail.com>
2022-09-23 05:48:28 +0300

    add file db.js

---
+++ shopping_cart/nodejs/server/db.js
@@ -0,0 +1,5 @@
+const {Sequelize} = require('sequelize') //импорт Sequelize + деструктуризация
+
+module.exports = new Sequelize(            //экспортируем объект который создаем их этого класса
+            
+) 
