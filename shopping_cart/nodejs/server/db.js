0c373baf411b7ec752dbe3d000b8c610b8c6b372
Stoian Iulia <iuliastoian8@gmail.com>
2022-09-23 07:52:26 +0300

    создание и объявление пременных БД

--- shopping_cart/nodejs/server/db.js
+++ shopping_cart/nodejs/server/db.js
@@ -1,5 +1,12 @@
 const {Sequelize} = require('sequelize') //импорт Sequelize + деструктуризация
 
-module.exports = new Sequelize(            //экспортируем объект который создаем их этого класса
-            
+module.exports = new Sequelize(   //экспортируем объект который создаем из этого класса
+    process.env.DB_NAME,     //название БД
+    process.env.DB_USER,     //пользователь
+    process.env.DB_PASSWORD,  //пароль
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,      //объект
+        port: process.env.DB_PORT
+    }
 ) 
