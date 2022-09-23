86c70865447cb6b270eb5d9245703a91a95f1eee
Stoian Iulia <iuliastoian8@gmail.com>
2022-09-23 04:58:32 +0300

    adding file .env

--- shopping_cart/nodejs/server/index.js
+++ shopping_cart/nodejs/server/index.js
@@ -1,7 +1,8 @@
+require("dotenv").config()
 const express = require('express') //с помощью require можно импортировать какие то модули в файл, в данном случае express
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000 //получаем порт из переменной окружения, если эта переменная не задана, тогда по умоолчанию будет порт 5000
 
 const app = express() //создаем объект вызвав функцию экспресс, с него бужет начинаться запуск нашего приложения
 
-app.listen(PORT, () => console.log(`Server started on port ${500}`)) //у app вызываем функцию listen, в которой указываем какой порт должен прослушивать наш сервер
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)) //у app вызываем функцию listen, в которой указываем какой порт должен прослушивать наш сервер
