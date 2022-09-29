c51aa8c8c1e105eecb7a7424a90e7158693b57d6
Stoian Iulia <iuliastoian8@gmail.com>
2022-09-29 16:59:26 +0300

    Error was tottaly fixing

--- shopping_cart/nodejs/server/models/models.js
+++ shopping_cart/nodejs/server/models/models.js
@@ -1,39 +1,46 @@
 const sequelize = require('../db')   //импортируем объект sequelize
 const {DataTypes, STRING} = require('sequelize')
 
-const User = sequelize.define( 'user',{  
+const User = sequelize.define( 'user',
+{  
     id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     email: {type: DataTypes.STRING, unique: true},    //должно быть уникальным, не может быть пустым
     password: {type: DataTypes.STRING},
     role: {type: DataTypes.STRING, defaultValue: "USER"},
 })
 
-const Basket = sequelize.define( 'basket',{  
+const Basket = sequelize.define( 'basket',
+{  
     id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 })
 
-const BasketDevice = sequelize.define( 'basket_device',{  
+const BasketDevice = sequelize.define( 'basket_device',
+{  
     id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 })
 
-const Device = sequelize.define( 'device',{  
+const Device = sequelize.define( 'device',
+{  
     id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false}, 
     price: {type: DataTypes.INTEGER, allowNull: false},
     img: {type: DataTypes.STRING, allowNull: false},   //в строке будем хранить название файла и его рпсширение
 })
 
-const Type = sequelize.define( 'type',{  
+const Type = sequelize.define( 'type',
+{  
     id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
 })
 
-const Brand = sequelize.define( 'brand',{  
+const Brand = sequelize.define( 'brand',
+{  
     id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
 })
 
-const DeviceInfo = sequelize.define( 'device_info',{  
+const DeviceInfo = sequelize.define( 'device_info',
+{  
     id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     title: {type: DataTypes.STRING, allowNull: false},
     description: {type: DataTypes.STRING, allowNull: false},
