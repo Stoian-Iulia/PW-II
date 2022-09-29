cc8d925fd013534772ee41fa3f8cfcea1c407043
Stoian Iulia <iuliastoian8@gmail.com>
2022-09-29 22:30:32 +0300

    union of routers

--- shopping_cart/nodejs/server/routes/index.js
+++ shopping_cart/nodejs/server/routes/index.js
@@ -1,9 +1,15 @@
 const Router = require('express')
-const router = new Router()
+const router = new Router
+const deviciRouter = require('./deviceRouter')
+const brandRouter = require('./brandRouter')
+const typeRouter = require('./typeRouter')
+const userRouter = require('./userRouter')
 
-router.use('/user')
-router.use('/type')
-router.use('/brand')
-router.use('/device')
+
+//объединение всех роутеров в один
+router.use('/user', userRouter)
+router.use('/type', typeRouter)
+router.use('/brand', brandRouter)
+router.use('/device', deviciRouter)
 
 module.exports = router
