cd646e1cfcea9e5ebd7231033b212fa7ac8a96a9
Stoian Iulia <iuliastoian8@gmail.com>
2022-10-07 05:01:20 +0300

    token generating, password hashing

--- shopping_cart/nodejs/server/controllers/userController.js
+++ shopping_cart/nodejs/server/controllers/userController.js
@@ -1,7 +1,27 @@
 const apiError = require('../error/apiError');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {User, Basket} = require('../models/models');
 
 class UserController {
-    async registration (req, res) {
+    async registration (req, res, next) {
+        const {email, password, role} = req.body
+        if (!email || !password) {
+            return next(apiError.badRequest('Некоректный email или password'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(apiError.badRequest('Пользователь с таким email уже сушествует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 3)
+        const user = await User.create({email, role, password, hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = jwt.sign(
+            {id: user.id, email, role},
+            process.env.SECRET_KEY,
+            {expiresIn: '24h'}
+        )
+        return res.json({token})
     }
 
     async login (req, res) {
